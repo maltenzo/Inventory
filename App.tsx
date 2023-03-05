9cc86bc72d9f75f7860bad5dc042312690e8b570
Enzo Vigneti <enzo@amalgama.co>
2023-03-05 12:25:57 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+import {Chip, Provider as PaperProvider} from 'react-native-paper';
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <PaperProvider>
+      <SafeAreaView style={backgroundStyle}>
+        <StatusBar
+          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={backgroundStyle.backgroundColor}
+        />
+        <Chip icon={'information'} onPress={() => console.log('preess')}>
+          Press me
+        </Chip>
+      </SafeAreaView>
+    </PaperProvider>
+  );
+}
+
+export default App;
